801f91c436766dffb417e2c23b10034dd9264b33
Goto Ippei <76621063+paaaaay5@users.noreply.github.com>
2022-08-26 14:04:08 +0900

    test

--- eval.py
+++ eval.py
@@ -2,25 +2,23 @@ import gensim
 from pprint import pprint
 from janome.tokenizer import Tokenizer
 import re
+import time
 import trans as ts
 
 eval = 0
 
-
 def main():
     # modelの読み込みに時間がかかる，事前に読んでデモするといいかも
     model_path = "model/chive-1.2-mc90.kv"
-
     # モデルの読み込み
     print("読み込み中")
     model = gensim.models.KeyedVectors.load(model_path)
-    print("あああああああああああああああああああ")
-
+    print("読み込み完了")
+    going_time = time.time()
     while(1):
         text = ts.main()
         if(text == -1):
             continue
-        # text = "今日はいい天気ですね"
         calc(text, model)
 
 
@@ -36,7 +34,7 @@ def calc(text, model):
     word_list = []
 
     # 炎上しそうなワード
-    fire_words = ['炎上', '人権', '動画', '拡散', '不正', '歌い手', '喧嘩','女','男',]
+    fire_words = ['炎上','人権','動画','拡散','不正','歌い手','喧嘩','女','男']
 
     for n in malist:
         if(regex.match(n.part_of_speech)):
@@ -46,15 +44,16 @@ def calc(text, model):
         max_fire = 0
         for eval_word in fire_words:
             try:
-                max_fire = model.similarity(eval_word, tmp_word)
+                max_fire = max(model.similarity(eval_word, tmp_word),max_fire)
             except:
-                max_fire = 0.1
+                max_fire = max(0.1,max_fire)
                 #print(eval_word,tmp_word)
+
         if (max_fire >= 0.2):
-            # eval += max_fire*100
-            eval += 12.5
+            eval += 10
         else:
             eval -= 5
+
     if(eval < 0):
         eval = 0
 
